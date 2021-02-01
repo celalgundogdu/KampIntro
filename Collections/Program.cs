28540443955e340836a24f1c0b9e4a02e10bb577
Celal Gündoğdu <celalgundogdu42@gmail.com>
2021-02-02 00:29:46 +0300

    4. Ders Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] { "Atiba", "Josef", "Aboubakar", "Rachid" };
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+
+            //names = new string[5]; // yeni adres olusur ve onceki verilere ulasılamaz
+            //names[4] = "";
+            //Console.WriteLine(names[4]);
+            //Console.WriteLine(names[0]); // null
+
+
+            List<string> names2 = new List<string> { "Atiba", "Josef", "Aboubakar", "Rachid" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+            names2.Add("Rosier");
+            Console.WriteLine(names2[4]);
+            Console.WriteLine(names2[0]);  
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Ati");
+            names.Add("Abu");
+
+
+            Console.Read();
+        }
+    }
+}
