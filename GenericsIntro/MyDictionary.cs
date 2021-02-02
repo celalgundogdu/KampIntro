ad59030bca07a84dec913601ac617ba9e52b02e1
Celal Gündoğdu <celalgundogdu42@gmail.com>
2021-02-02 12:05:05 +0300

    Dictionary-HW

---
+++ GenericsIntro/MyDictionary.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyDictionary<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            if (Search(key) == false)
+            {
+                K[] tempKeys = keys;
+                V[] tempValues = values;
+
+                keys = new K[keys.Length + 1];
+                values = new V[values.Length + 1];
+
+                for (int i = 0; i < tempKeys.Length; i++)
+                {
+                    keys[i] = tempKeys[i];
+                    values[i] = tempValues[i];
+                }
+
+                keys[keys.Length - 1] = key;
+                values[values.Length - 1] = value;
+            }
+            else
+            {
+                Console.WriteLine("This key can not used since it is already in the dictionary");
+            }
+            
+        }
+
+        public bool Search (K key) 
+        {
+            bool isExist = false;
+            foreach (K item in keys)
+            {
+                if (item.Equals(key))
+                {
+                    isExist = true;
+                    break;
+                }
+            }
+            return isExist;
+        }
+
+        public K[] Keys
+        {
+            get { return keys; }
+        }
+
+        public V[] Values
+        {
+            get { return values; }
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " - " + values[i]);
+            }
+        }
+
+    }
+}
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -6,10 +6,29 @@ namespace GenericsIntro
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("---List---");
             MyList<string> names = new MyList<string>();
             names.Add("Ati");
             names.Add("Abu");
 
+            Console.WriteLine(names.Length);
+
+            foreach (var name in names.Items)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("---Dictionary---");
+            MyDictionary<int, string> players = new MyDictionary<int, string>();
+            players.Add(2, "Rosier");
+            players.Add(24, "Vida");
+            players.Add(4, "Montero");
+
+            players.print();
+
+            players.Add(4, "Welinton");
+
+            players.print();
 
             Console.Read();
         }
