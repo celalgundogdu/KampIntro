3c23c47b7ec3490187c3f81db88c2989eaeae00d
Celal Gündoğdu <celalgundogdu42@gmail.com>
2021-02-12 00:05:07 +0300

    5. Ders Odev

--- GameProjectDemo/Abstract/ICampaignService.cs
+++ GameProjectDemo/Abstract/ICampaignService.cs
@@ -8,7 +8,7 @@ namespace GameProjectDemo.Abstract
     public interface ICampaignService
     {
         void Add(Campaign campaign);
-        void Remove(Campaign campaign));
-        void Update(Campaign campaign));
+        void Remove(Campaign campaign);
+        void Update(Campaign campaign);
     }
 }
